d43abeee9abe83771ae0969bd102040f7085cb5f
Bryce Thornton <brycethornton@gmail.com>
2011-09-27 02:40:54 -0400

    In the middle of trying to figure out the multi-account balance code.

--- lib/rebalance/target.rb
+++ lib/rebalance/target.rb
@@ -1,19 +1,23 @@
 module Rebalance
   class Target
     attr_accessor :asset_classes,
-                  :rebalanced_shares,
-                  :rebalanced_values,
-                  :rebalanced_share_difference,
-                  :rebalanced_value_difference
+      :rebalanced_shares,
+      :rebalanced_values,
+      :rebalanced_share_difference,
+      :rebalanced_value_difference
 
     def initialize(&block)
       self.asset_classes = {}
+      initialize_result_values
+
+      instance_eval &block
+    end
+
+    def initialize_result_values
       self.rebalanced_shares = {}
       self.rebalanced_values = {}
       self.rebalanced_share_difference = {}
       self.rebalanced_value_difference = {}
-
-      instance_eval &block
     end
 
     def asset_class(percentage, asset_class)
@@ -28,24 +32,77 @@ module Rebalance
       output
     end
 
-    def calculate_target_asset_class_values(account)
+    def calculate_target_asset_class_values(*accounts)
+      total_value = total_value_of_all_accounts(accounts)
+
       target_values = {}
       asset_classes.each do |asset_class, percentage|
-        target_values[asset_class] = (account.total_value * (percentage.to_f/100)).round(2)
+        target_values[asset_class] = (total_value * (percentage.to_f/100)).round(2)
       end
       target_values
     end
 
-    def calculate_current_asset_class_values(account)
+    def calculate_current_asset_class_values(*accounts)
       current_values = {}
-      account.funds.each do |symbol, fund|
-        current_values[fund.asset_class] = 0 if current_values[fund.asset_class].nil?
-        current_values[fund.asset_class] += fund.value
+      accounts.each do |account|
+        account.funds.each do |symbol, fund|
+          current_values[fund.asset_class] = 0 if current_values[fund.asset_class].nil?
+          current_values[fund.asset_class] += fund.value
+        end
       end
       current_values
     end
 
-    def rebalance(account)
+    def calculate_current_asset_class_percentages(*accounts)
+      current_percentages = {}
+      current_values = calculate_current_asset_class_values(*accounts)
+      total_value = total_value_of_all_accounts(accounts)
+
+      current_values.each do |asset_class, asset_class_value|
+        current_percentages[asset_class] = ((asset_class_value / total_value)*100).round(4)
+      end
+      current_percentages
+    end
+
+    def total_value_of_all_accounts(accounts)
+      value = 0
+      accounts.each do |account|
+        value += account.total_value
+      end
+      value
+    end
+
+    # get each account's asset class percentage breakdown in relation
+    # to all the accounts
+    def asset_class_percentages_across_all_accounts(accounts)
+      total_value_of_all_accounts = total_value_of_all_accounts(accounts)
+      account_percentages = {}
+
+      accounts.each do |account|
+        account.funds.each do |symbol, fund|
+          asset_class_total = 0
+          account.find_by_asset_class(fund.asset_class).each do |asset_class_fund|
+            asset_class_total += asset_class_fund.value
+          end
+          account_percentages[account.name] = {} if account_percentages[account.name].nil?
+          account_percentages[account.name][fund.asset_class] = ((asset_class_total / total_value_of_all_accounts) * 100).round(4)
+        end
+      end
+      account_percentages
+    end
+
+    def rebalance(*accounts)
+      initialize_result_values
+
+      if accounts.size == 1
+        single_account_rebalance(accounts.first)
+      elsif accounts.size > 1
+        multiple_account_rebalance(accounts)
+      end
+    end
+
+    private
+    def single_account_rebalance(account)
       target_asset_class_values   = calculate_target_asset_class_values(account)
       current_asset_class_values  = calculate_current_asset_class_values(account)
 
@@ -69,6 +126,58 @@ module Rebalance
           end
         end
       end
+    end
+
+    def multiple_account_rebalance(accounts)
+      working_account_percentages = asset_class_percentages_across_all_accounts(accounts)
+      working_asset_class_percentages = calculate_current_asset_class_percentages(*accounts)
+
+      p working_account_percentages
+      p working_asset_class_percentages
+
+      100.times do |num|
+        working_account_percentages.each do |account_name, a_classes|
+          # We need to deal with single fund accounts seperately
+          if a_classes.size > 1
+            a_classes.each do |class_name, percentage|
+              target_value = asset_classes[class_name] || 0
+              diff = target_value - working_asset_class_percentages[class_name]
+
+              #p "Target for #{class_name} is #{target_value}"
+
+              # See if the target asset class % is greater than our overall asset class %
+              if diff
+                if diff > 0
+                  diff > 1 ? adjustment_size = 1 : adjustment_size = diff
+                else
+                  diff < -1 ? adjustment_size = -1 : adjustment_size = diff
+                end
+
+                if adjustment_size > 0.1 or adjustment_size < -0.1
+                  #p ""
+                  #p "Adjusting #{class_name} in #{account_name} by #{adjustment_size} because it's currently at #{working_asset_class_percentages[class_name]}"
+                  working_account_percentages[account_name][class_name] += adjustment_size
+                  working_asset_class_percentages[class_name] += adjustment_size
+
+                  # Now, go through the rest of the asset classes here and decrement
+                  # evenly to keep this account's overall percentage the same
+
+                  percent_to_change = adjustment_size.quo(a_classes.size-1).to_f * -1
+                  a_classes.each do |temp_class_name, temp_percentage|
+                    next if temp_class_name == class_name
+                    #p "Minor adjustment to #{temp_class_name} in #{account_name} by #{percent_to_change}"
+                    working_account_percentages[account_name][temp_class_name] += percent_to_change
+                    working_asset_class_percentages[temp_class_name] += percent_to_change
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+      p working_account_percentages
+      p working_asset_class_percentages
+      working_account_percentages
     end
   end
 end
--- spec/rebalance/target_spec.rb
+++ spec/rebalance/target_spec.rb
@@ -110,4 +110,115 @@ describe Rebalance::Target do
       end
     end
   end
+
+  describe 'with multiple accounts' do
+    before do
+      @wifes_roth = Rebalance::Account.new "Wife's Roth" do
+        fund 'ABCDE', 'Some Asset Class', 500, 10.00 # $5,000
+        fund 'FGHIJ', 'Some Asset Class', 300, 25.00 # $7,500
+        fund 'KLMNO', 'Another Asset Class', 75, 300 # $22,500
+        fund 'PQRST', 'Bonds', 35.5, 32.00           # $1,136
+        fund 'UVWXY', 'Bonds', 75, 5.50              # $412.50
+      end
+
+      @my_roth = Rebalance::Account.new 'My Roth' do
+        fund 'AAAAA', 'Cash', 150, 1.00              # $150
+        fund 'BBBBB', 'Some Asset Class', 10, 23.00  # $230
+        fund 'FGHIJ', 'Some Asset Class', 100, 25.00 # $2,500
+      end
+
+      @my_sep_ira = Rebalance::Account.new 'My SEP IRA' do
+        fund 'ZZZZZ', 'Bonds', 250, 20.25            # $5,062.50
+      end
+    end
+
+    it 'calculates the total value of all accounts' do
+      @target.total_value_of_all_accounts([@wifes_roth, @my_roth, @my_sep_ira]).must_equal 44491.00
+    end
+
+    it 'calculates the target value for each asset class' do
+      target_values = @target.calculate_target_asset_class_values(@wifes_roth, @my_roth, @my_sep_ira)
+
+      expected_target_values = {
+        'Some Asset Class' => 13347.30,
+        'Another Asset Class' => 8898.20,
+        'Bonds' => 22245.50
+      }
+
+      target_values.must_equal expected_target_values
+    end
+
+    it 'calculates the current value for each asset class' do
+      current_values = @target.calculate_current_asset_class_values(@wifes_roth, @my_roth, @my_sep_ira)
+
+      expected_current_values = {
+        'Some Asset Class' => 15230.00,
+        'Another Asset Class' => 22500.00,
+        'Bonds' => 6611.00,
+        'Cash' => 150.00
+      }
+
+      current_values.must_equal expected_current_values
+    end
+
+    it 'calculates the current percentage for each asset class' do
+      current_values = @target.calculate_current_asset_class_percentages(@wifes_roth, @my_roth, @my_sep_ira)
+
+      expected_current_values = {
+        'Some Asset Class' => 34.2316,
+        'Another Asset Class' => 50.5720,
+        'Bonds' => 14.8592,
+        'Cash' => 0.3371
+      }
+
+      current_values.must_equal expected_current_values
+    end
+
+    it 'calculates the asset class percentages across all accounts' do
+      expected_percentages = {
+        "Wife's Roth" => {
+          "Some Asset Class" => 28.0956,
+          "Another Asset Class" => 50.5720,
+          "Bonds" => 3.4805
+        },
+        "My Roth" => {
+          "Cash" => 0.3371,
+          "Some Asset Class" => 6.1361
+        },
+        "My SEP IRA" => {
+          "Bonds" => 11.3787
+        }
+      }
+
+      @target.asset_class_percentages_across_all_accounts([@wifes_roth, @my_roth, @my_sep_ira]).must_equal expected_percentages
+    end
+
+    describe 'after a rebalance' do
+      before do
+        @target.rebalance(@wifes_roth, @my_roth, @my_sep_ira)
+      end
+
+      it 'provides the new number of shares for each fund' do
+        expected_rebalance = {
+          "Wife's Roth" => {
+            'ABCDE' => 548.2275,
+            'FGHIJ' => 219.291,
+            'KLMNO' => 24.3657,
+            'PQRST' => 285.5352,
+            'UVWXY' => 1661.2955
+          },
+          "My Roth" => {
+            'AAAAA' => 0,
+            'BBBBB' => 0,
+            'FGHIJ' => 0
+          },
+          "My SEP IRA" => {
+            'ZZZZZ' => 0
+          }
+        }
+
+        @target.rebalanced_shares.must_equal expected_rebalance
+      end
+    end
+  end
 end
